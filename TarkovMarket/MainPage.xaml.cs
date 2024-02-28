7224a23068a889dd9c4855da772251e0a2dce1ed
CloodDev <112552341+CloodDev@users.noreply.github.com>
2024-02-28 15:59:43 +0100

    update

--- TarkovMarket/MainPage.xaml.cs
+++ TarkovMarket/MainPage.xaml.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 using Xamarin.Forms;
+using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace TarkovMarket
 {
@@ -13,6 +16,40 @@ namespace TarkovMarket
         public MainPage()
         {
             InitializeComponent();
+            GetDataFromWebApi("PACA");
         }
+
+        private async Task<string> GetDataFromWebApi(string item)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var query = new
+                {
+                    query = @"
+                        query ($item: String!) {
+                            item(name: $item) {
+                                name
+                                price
+                                traderPrices {
+                                    trader
+                                    price
+                                }
+                            }
+                        }",
+                    variables = new { item }
+                };
+
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(query);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync("https://api.tarkov.dev/graphql", content);
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                // Process the responseContent here
+                Debug.WriteLine(responseContent);
+                return responseContent;
+            }
+        }
+
     }
 }
